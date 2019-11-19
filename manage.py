d02511d629111f5dda4d7e33eb5ed43fa66efea4
lengai <371522155@qq.com>
2019-11-19 19:33:07 +0800

    命令行下用户注册

--- manage.py
+++ manage.py
@@ -4,6 +4,7 @@ import os
 
 from flask import request
 from flask.cli import load_dotenv
+from app import models
 
 from app import create_app
 
@@ -24,3 +25,24 @@ def before_request():
     else:
         app.jinja_loader.searchpath = app.jinja_loader.searchpath[-1:]
 
+
+@app.cli.command("create-user")
+def create_user():
+    """
+    创建用户
+    :return:
+    """
+    from app.services import user_srv
+
+    print('开始创建账号')
+    phone = input("输入用户的手机号:")
+
+    data = user_srv.get_first({"phone": phone})
+
+    while data is not None:
+        phone = input("该号码已经存在了,请重新输入号码:")
+        data = user_srv.get_first({"phone": phone})
+
+    password = input("请输入登陆密码:")
+
+    user_srv.save(**{"phone": phone, "password": password})
